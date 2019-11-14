1b78ca9aa3acc17dd5e3ab13dd4790c3d4db9e1b
kphung29 <kphung29@gmail.com>
2019-11-13 20:34:01 -0500

    setting up mongoose and mongo atlas cluster

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 const express = require("express");
+const cors = require("cors");
+const db = require("../database/index");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+app.use(express.json());
 app.use(express.static(`${__dirname}/../client/dist`));
 
 // app.get("/", (req, res) => {
