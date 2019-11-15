3f945606082780ea80b530cd8d62b9a010d3e2f5
kphung29 <kphung29@gmail.com>
2019-11-15 12:18:14 -0500

    created components for front end

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import ExerciseList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercises";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <Router>
