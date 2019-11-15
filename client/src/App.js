4ab84621967347a9831b55a9fe5d49808baddc34
kphung29 <kphung29@gmail.com>
2019-11-15 11:45:54 -0500

    creating Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
 };
 
 export default App;
