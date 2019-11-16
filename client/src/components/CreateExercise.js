1178cebefafd252a3189d5abe1785eec168ea830
kphung29 <kphung29@gmail.com>
2019-11-15 21:07:10 -0500

    building out CreateExercise component

--- client/src/components/CreateExercise.js
+++ client/src/components/CreateExercise.js
@@ -1,14 +1,124 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/no-string-refs */
 /* eslint-disable react/jsx-filename-extension */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable react/require-render-return */
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: []
+    };
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      users: ["test user"],
+      username: "test user"
+    });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const { username, description, duration, date } = this.state;
+    const exercise = {
+      username,
+      description,
+      duration,
+      date
+    };
+
+    console.log(exercise);
+
+    window.location = "/";
+  }
+
   render() {
+    const { username, users, duration, date } = this.state;
     return (
       <div>
-        <p>This is CreateExercise component!</p>
+        <h3>Create New Exercise Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={username}
+              onChange={this.onChangeUsername}
+            >
+              {users.map(user => (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker selected={date} onChange={this.onChangeDate} />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
